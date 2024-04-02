b6c40c9b6c7dd5f4d5eb6fd9f8e7d2c04ee5a980
Ibrahim Daniel <daniel.ibrahim@student.supsi.ch>
2024-04-02 22:10:25 +0200

    toggle button

--- chat/src/app/search-message/search-message.component.ts
+++ chat/src/app/search-message/search-message.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter } from '@angular/core';
 import { Output } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-message',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-message.component.html',
   styleUrl: './search-message.component.css'
 })
@@ -13,10 +13,16 @@ export class SearchMessageComponent {
 
   filterChat: string;
   @Output() filterMessageToChat = new EventEmitter<string>();
+  show: boolean = true;
+
 
   postFilter(filterChoose: string){
     this.filterChat = filterChoose;
     this.filterMessageToChat.emit(this.filterChat);
+  }
+
+  toggleVisibility(): void {
+    this.show = !this.show;
   }
 
 }
